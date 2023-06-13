3c0bc6bec4c12fb01c8a173e8057cb21c02c4071
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 20:26:51 +0200

    add upload test - upload file

--- src/test/java/pl/grzegorzworek/selenide/automationbro/UploadTest.java
+++ src/test/java/pl/grzegorzworek/selenide/automationbro/UploadTest.java
@@ -2,11 +2,15 @@ package pl.grzegorzworek.selenide.automationbro;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class UploadTest {
     @Test
     public void testUploadFileOnVisibleInput() {
         open("https://the-internet.herokuapp.com/upload");
+        $("#file-upload").uploadFile(new File("src/test/java/pl/grzegorzworek/selenide/data/sample.png"));
     }
 }
