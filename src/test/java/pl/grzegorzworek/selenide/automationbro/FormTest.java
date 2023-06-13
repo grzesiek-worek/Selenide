196ad30da9213c969e37953ab04508b52b43fc75
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 19:54:23 +0200

    add formTest - find and fill input elements

---
+++ src/test/java/pl/grzegorzworek/selenide/automationbro/FormTest.java
@@ -0,0 +1,19 @@
+package pl.grzegorzworek.selenide.automationbro;
+
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormTest {
+    @Test
+    public void testFormFields() {
+        open("https://practice.automationbro.com/support-form");
+
+        $(".support-name input").val("Greg");
+        $(".support-email input").val("test@gmail.com");
+        $(".support-subject input").val("Need help with repairing my shop");
+
+
+    }
+}
