f2227dab1d2ba8f43b7ea89152886a6353e35057
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 16:35:41 +0200

    add shouldhave result text

--- src/test/java/pl/grzegorzworek/selenide/google/GoogleSearchTest.java
+++ src/test/java/pl/grzegorzworek/selenide/google/GoogleSearchTest.java
@@ -1,19 +1,22 @@
 package pl.grzegorzworek.selenide.google;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class GoogleSearchTest extends Base{
 
     @Test
     public void searchKeywordWithGoogle(){
 
-        $(By.id("W0wltc")).click(); // cookies decline
+        $(By.id("W0wltc")).click(); // cookies reject button
 
         $(By.name("q")).setValue("Selenide").pressEnter();
+
+        $(By.tagName("h3")).shouldHave(Condition.exactText("Selenide: concise UI tests in Java"));
     }
 }
