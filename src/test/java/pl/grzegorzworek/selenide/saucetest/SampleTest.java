af7ca7276696628bf069b2ec1990b14199270f49
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 11:39:23 +0200

    add Sample test - login usur and check title page

--- src/test/java/pl/grzegorzworek/selenide/saucetest/SampleTest.java
+++ src/test/java/pl/grzegorzworek/selenide/saucetest/SampleTest.java
@@ -1,2 +1,18 @@
-package pl.grzegorzworek.selenide.saucetest;public class SampleTest {
+package pl.grzegorzworek.selenide.saucetest;
+
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class SampleTest extends Base{
+
+    @Test
+    public void testLoginInAsValidUser(){
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+        $x(".//span[@class='title']").shouldHave(Condition.exactText("PRODUCTS"));
+    }
 }
