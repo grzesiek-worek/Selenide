f688ab1efeb388486e7817981bed93204eb8444e
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 20:45:23 +0200

    upload test - click the upload button

--- src/test/java/pl/grzegorzworek/selenide/automationbro/UploadTest.java
+++ src/test/java/pl/grzegorzworek/selenide/automationbro/UploadTest.java
@@ -26,5 +26,7 @@ public class UploadTest {
         executeJavaScript("document.getElementById(\"upfile_1\").classList.remove(\"file_input_hidden\")");
 
         $("#upfile_1").uploadFile(new File("src/test/java/pl/grzegorzworek/selenide/data/sample.png"));
+
+        $("#upfile_1").click();
     }
 }
