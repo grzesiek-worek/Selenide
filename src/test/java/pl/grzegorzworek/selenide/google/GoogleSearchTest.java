c248384684a08a0b943af18e15452b57ca6d9194
grzesiek-worek <grzesiekw@interia.eu>
2023-06-13 15:39:07 +0200

    add test searchKeywordWithGoogle

--- src/test/java/pl/grzegorzworek/selenide/google/GoogleSearchTest.java
+++ src/test/java/pl/grzegorzworek/selenide/google/GoogleSearchTest.java
@@ -1,5 +1,17 @@
 package pl.grzegorzworek.selenide.google;
 
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class GoogleSearchTest extends Base{
 
+    @Test
+    public void searchKeywordWithGoogle(){
+
+        $(By.name("q")).setValue("Selenide").pressEnter();
+    }
 }
